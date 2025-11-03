aa7d3410c5ea3cd7e32a8e9f27d53cbd0e282eb5
mktdgustavo-cmyk <mktdgustavo@gmail.com>
2025-11-02 22:04:50 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -682,7 +682,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
